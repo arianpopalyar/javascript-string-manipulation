30c7c2abb7ba90482883b505a5b3ae19e84e9a57
arianpopalyar <arian_popalyar@yahoo.com>
2022-06-10 22:04:34 +0100

    adding tests

--- src/index.spec.js
+++ src/index.spec.js
@@ -6,19 +6,19 @@ describe("CRLF Converter", () => {
   describe("lf", () => {
     it("should replace CRLF at the beginning of a string", () => {
       const result = lf`\r\nHello there.`;
-
+      String.prototype.startsWith(result)
       // Use `String.prototype.startsWith()`
     });
 
     it("should replace CRLF at the end of a string", () => {
       const result = lf`Hello there.\r\n`;
-
+      String.prototype.endsWith(result);
       // Use `String.prototype.endsWith()`
     });
 
     it("should replace CRLF in the middle of a string", () => {
       const result = lf`Hello \r\n\r\nthere.`;
-
+      String.prototype.includes(result);
       // Use `String.prototype.includes()`
     });
   });
