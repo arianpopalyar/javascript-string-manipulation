01a1847604c4f5d2537f5ef83e45336cebf6f406
arianpopalyar <arian_popalyar@yahoo.com>
2022-06-10 22:11:08 +0100

    addind the test

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-unused-vars */
+const assert = require("chai");
 const { describe, it } = require("mocha");
 const { lf } = global.lf || require("index.js");
 
@@ -6,19 +7,19 @@ describe("CRLF Converter", () => {
   describe("lf", () => {
     it("should replace CRLF at the beginning of a string", () => {
       const result = lf`\r\nHello there.`;
-      String.prototype.startsWith(result)
+      assert(result.startsWith("\nHello"));
       // Use `String.prototype.startsWith()`
     });
 
     it("should replace CRLF at the end of a string", () => {
       const result = lf`Hello there.\r\n`;
-      String.prototype.endsWith(result);
+      assert(result.endsWith("\n"))
       // Use `String.prototype.endsWith()`
     });
 
     it("should replace CRLF in the middle of a string", () => {
       const result = lf`Hello \r\n\r\nthere.`;
-      String.prototype.includes(result);
+      assert(result.includes("\n\n"));
       // Use `String.prototype.includes()`
     });
   });
